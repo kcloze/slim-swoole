ea8e18ecac6d1c752a54a24b733f8fd9c55fba42
lixiaopei <lixiaopei@yaochufa.com>
2017-03-23 19:07:52 +0800

    router

--- app/config/routes.php
+++ app/config/routes.php
@@ -8,6 +8,12 @@
  * with this source code in the file LICENSE.
  */
 
+// use Psr\Http\Message\ResponseInterface as Response;
+// use Psr\Http\Message\ServerRequestInterface as Request;
+// $app->get('/{contr}/{act}[/{args:*}]', function (Request $request, Response $response, $args = []) {
+//     return $this->get($args['contr'])->{$act}($request, $response, $args);
+// });
+
 $app->get('/', '\App\Controllers\DefaultController:index');
 $app->get('/index', '\App\Controllers\DefaultController:index');
-$app->get('/oop', '\App\Controllers\DefaultController:oop');
+$app->get('/default/oop', '\App\Controllers\DefaultController:oop');
